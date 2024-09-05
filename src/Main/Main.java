8740fdc1ca6ce3a89d0a54463637b65a880b6bf5
Lucian <llandey8161@student.pps.net>
2024-09-05 09:41:25 -0700

    test

---
+++ src/Main/Main.java
@@ -0,0 +1,15 @@
+package Main;
+
+import javax.swing.JFrame;
+
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Adventure Fun");
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
