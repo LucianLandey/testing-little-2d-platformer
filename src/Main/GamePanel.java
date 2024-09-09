61bab8a0c2406fb48b2475e3c25c341f978bfc1d
Lucian <llandey8161@student.pps.net>
2024-09-09 09:47:31 -0700

    updated

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -1,9 +1,55 @@
 package Main;
 
-import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
 
-public class GamePanel {
+public class GamePanel extends JPanel implements Runnable{
     //SCREEN SETTINGS
-    final int originalTileSize = 16; //16x16 tiles
+    final int originalTileSize = 16; //16x16 tile
+    final int scale = 3;
 
+    final int tileSize = originalTileSize * scale; //48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 px
+    final int screenHeight = tileSize * maxScreenRow; // 576 px
+
+    Thread gameThread;
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+    public void startGameThread() {
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    @Override
+    public void run() {
+
+            while(gameThread != null) {
+
+                //1 Update: update info such as character positions
+                update();
+                //2 DRAW: draw the screen with the udated info
+                repaint();
+            }
+    }
+    public void update() {
+
+    }
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.dispose();
+    }
 }
